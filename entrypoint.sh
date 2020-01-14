37d0d02530b404c571cbf6a00fc14b7291ab7e9d
thiagopnobre <thiago_pnobre@hotmail.com>
2020-01-14 19:07:44 -0300

    Refactor  entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,19 @@ then
   mkdir /capybara_evaluator_action/spec/$GITHUB_REPOSITORY -p
   mv ./* /capybara_evaluator_action/spec/$GITHUB_REPOSITORY/
   cd /capybara_evaluator_action
+else
+  mv /capybara_evaluator_action/spec/$GITHUB_REPOSITORY/requirements_mapping.json .
 fi
 
 bundle exec rspec --format json --out evaluation.json
 
-if [ $GITHUB_ACTIONS ]
+ruby evaluator.rb
+
+if [ $? != 0 ]
 then
-  ruby evaluator.rb
-  echo ::set-output name=result::`cat result.json | base64 -w 0`
-else
-  echo ::set-output name=evaluation::`cat evaluation.json | base64 -w 0`
+  echo "Execution error"
+  exit 1
 fi
+
+echo ::set-output name=result::`cat result.json | base64 -w 0`
+echo ::set-output name=evaluation::`cat evaluation.json | base64 -w 0`
