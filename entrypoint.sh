77399074787da5a6663781a2739b0e096ba18dd6
thiagopnobre <thiago_pnobre@hotmail.com>
2020-01-23 18:00:33 -0300

    Revert "Refactor  entrypoint"
    This reverts commit 37d0d02530b404c571cbf6a00fc14b7291ab7e9d.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,19 +6,14 @@ then
   mkdir /capybara_evaluator_action/spec/$GITHUB_REPOSITORY -p
   mv ./* /capybara_evaluator_action/spec/$GITHUB_REPOSITORY/
   cd /capybara_evaluator_action
-else
-  mv /capybara_evaluator_action/spec/$GITHUB_REPOSITORY/requirements_mapping.json .
 fi
 
 bundle exec rspec --format json --out evaluation.json
 
-ruby evaluator.rb
-
-if [ $? != 0 ]
+if [ $GITHUB_ACTIONS ]
 then
-  echo "Execution error"
-  exit 1
+  ruby evaluator.rb
+  echo ::set-output name=result::`cat result.json | base64 -w 0`
+else
+  echo ::set-output name=evaluation::`cat evaluation.json | base64 -w 0`
 fi
-
-echo ::set-output name=result::`cat result.json | base64 -w 0`
-echo ::set-output name=evaluation::`cat evaluation.json | base64 -w 0`
