9b438796569f7f0a6699bd0b61977a2a5c290ada
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-01-24 14:39:52 -0300

    put hardcoded project reference

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,15 @@
 
 if [ $GITHUB_ACTIONS ]
 then
-  git clone https://github.com/$GITHUB_REPOSITORY-tests.git /project-tests
+  git clone https://github.com/tryber/sd-03-week3-html-css-project-tests.git /project-tests
   rm -rf /project-tests/.git
   cp -r /project-tests/* .
   mv requirements_mapping.json /capybara_evaluator_action/
-  mkdir /capybara_evaluator_action/spec/$GITHUB_REPOSITORY -p
-  mv ./* /capybara_evaluator_action/spec/$GITHUB_REPOSITORY/
+  mkdir /capybara_evaluator_action/spec/sd-03-week3-html-css-project -p
+  mv ./* /capybara_evaluator_action/spec/sd-03-week3-html-css-project/
   cd /capybara_evaluator_action
 else
-  mv /capybara_evaluator_action/spec/$GITHUB_REPOSITORY/requirements_mapping.json .
+  mv /capybara_evaluator_action/spec/tryber/sd-03-week3-html-css-project/requirements_mapping.json .
 fi
 
 bundle exec rspec --format json --out evaluation.json
