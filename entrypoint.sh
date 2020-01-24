fc92fe58616c682055d292253db0e97a8d41f02f
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-01-24 16:49:13 -0300

    add echo commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,6 @@ fi
 
 cat result.json
 
-# echo ::set-output name=evaluation::`cat evaluation.json | base64 -w 0`
-# echo ::set-output name=result::`cat result.json | base64 -w 0`
-# echo ::set-output name=pr-number::$(echo "$GITHUB_REF" | awk -F / '{print $3}')
+echo ::set-output name=evaluation::`cat evaluation.json | base64 -w 0`
+echo ::set-output name=result::`cat result.json | base64 -w 0`
+echo ::set-output name=pr-number::$(echo "$GITHUB_REF" | awk -F / '{print $3}')
