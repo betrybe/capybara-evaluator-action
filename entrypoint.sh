5a06283e79f49f55c34aea9be6635077c3433303
thiagopnobre <thiago_pnobre@hotmail.com>
2020-01-24 19:33:41 -0300

    Try to fix paths with env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 
 if [ $GITHUB_ACTIONS ]
 then
-  git clone https://github.com/$GITHUB_REPOSITORY-tests.git tests-repo
-  mv tests-repo/requirements_mapping.json /capybara_evaluator_action/
-  mkdir /capybara_evaluator_action/spec/project-repo -p
-  mv ./* /capybara_evaluator_action/spec/project-repo/
+  git clone https://github.com/$GITHUB_REPOSITORY-tests.git $GITHUB_REPOSITORY-tests
+  mv $GITHUB_REPOSITORY-tests/requirements_mapping.json /capybara_evaluator_action/
+  mkdir /capybara_evaluator_action/spec/$GITHUB_REPOSITORY -p
+  mv ./* /capybara_evaluator_action/spec/$GITHUB_REPOSITORY/
   cd /capybara_evaluator_action
 else
   mv /capybara_evaluator_action/spec/$GITHUB_REPOSITORY/$GITHUB_REPOSITORY-tests/requirements_mapping.json .
 fi
 
-GITHUB_REPOSITORY=project-repo bundle exec rspec --format json --out evaluation.json
+bundle exec rspec --format json --out evaluation.json
 
 ruby evaluator.rb
 
